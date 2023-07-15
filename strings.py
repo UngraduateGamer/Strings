b2bfdc4a63a39aa2c3892a2540a0ef292f9c481f
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-15 18:57:58 +0530

    strings slicing.py
    we can use simple strings and the slicing of the string so lets can play of the strings

--- strings.py
+++ strings.py
@@ -10,8 +10,16 @@ print(story)
 # r a h u l
 # 0 1 2 3 4
 # -5 -4 -3 -2 -1
+
 print(name[-5])
 print(name[1])
 print(name[2])
 print(name[3])
 print(name[-1])
+
+print(name[-4:-2]) 
+# or lower is the same output
+print(name[len(name)-4:len(name)-2])
+
+# slicing
+print(name[0:5]) # 0 inclluding 4 excluding
