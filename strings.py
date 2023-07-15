7e0ab98c7111f8b97b36912918e4f49520461cd6
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-15 18:39:58 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,17 @@
+name="Rahul"
+""" triple single or double quotes used for multiple line strings and comments """
+story=""" Twinkle Twinkle Little Star
+How are Wonder what you are
+"""
+print(story)
+
+# # index
+# string is the sequence of characters 
+# r a h u l
+# 0 1 2 3 4
+# -5 -4 -3 -2 -1
+print(name[-5])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[-1])
